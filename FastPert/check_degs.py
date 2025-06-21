464c44918f6a6a9afbc18b9e890c392af9d3ef97
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-21 12:50:21 -0400

    Update check_degs.py

--- FastPert/check_degs.py
+++ FastPert/check_degs.py
@@ -6,7 +6,7 @@ drug = st.text_input("Enter drug name:")
 cell_line = st.selectbox("Select Cell Line", df["cell_line"].unique())
 type = st.selectbox("Select data type", ['chemical','genetic'])
 
-dat = pd.read_csv(f'data/{type}_top20degs_{cell_line}.csv'), index_col=0)
+dat = pd.read_csv(f'data/{type}_top20degs_{cell_line}.csv', index_col=0)
 
 # Filter results
 dat = df[df["condition"] == drug]
