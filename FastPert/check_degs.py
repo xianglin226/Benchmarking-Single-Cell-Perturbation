b965183e6e6cbff7c94b59d92df7a0ec237493da
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-21 12:57:23 -0400

    Update check_degs.py

--- FastPert/check_degs.py
+++ FastPert/check_degs.py
@@ -6,9 +6,9 @@ drug = st.text_input("Enter drug name: (e.g. pazopanib...)")
 cell_line = st.selectbox("Select Cell Line", ['A549','MCF7','PC3', 'VCAP', 'MDAMB231', 'BT20', 'HT29', 'A375', 'HELA'])
 type = st.selectbox("Select data type", ['chemical','genetic'])
 
-if st.button("Load Local CSV"):
+if st.button("Load DEGs"):
     dat = pd.read_csv(f'data/{type}_top20degs_{cell_line}.csv', index_col=0)
     dat = df[df["condition"] == drug]
     st.write(f"Showing top 10 DEGs for cell line {cell_line} with {type} perturbagen: {drug}")
-    st.dataframe(dat[['names','logfoldchanges']])
+    st.dataframe(dat)
 
