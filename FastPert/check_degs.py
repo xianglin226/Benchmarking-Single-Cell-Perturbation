661b1a731d81b8f52485889650e3a20acd747fca
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-21 12:46:25 -0400

    Create check_degs.py

---
+++ FastPert/check_degs.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+# Sidebar inputs
+drug = st.text_input("Enter drug name:")
+cell_line = st.selectbox("Select Cell Line", df["cell_line"].unique())
+type = st.selectbox("Select data type", ['chemical','genetic'])
+
+dat = pd.read_csv(f'data/{type}_top20degs_{cell_line}.csv'), index_col=0)
+
+# Filter results
+dat = df[df["condition"] == drug]
+
+# Show DEGs
+st.write(f"Showing top 10 DEGs for cell line {cell_line} with {type} perturbagen: {drug}")
+st.dataframe(dat[['names','logfoldchanges']])
