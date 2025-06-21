c5d51e594c9e2ce4c2eb3beca6a9ae4a14d3bd92
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-21 13:04:59 -0400

    Update check_degs.py

--- FastPert/check_degs.py
+++ FastPert/check_degs.py
@@ -7,7 +7,7 @@ cell_line = st.selectbox("Select Cell Line", ['A549','MCF7','PC3', 'VCAP', 'MDAM
 type = st.selectbox("Select data type", ['chemical','genetic'])
 
 if st.button("Load DEGs"):
-    file_path = f"/mount/src/benchmarking-single-cell-perturbation/FastPert/Data/{type}_top20degs_{cell_line}.csv"
+    file_path = f"/mount/src/benchmarking-single-cell-perturbation/FastPert/data/{type}_top20degs_{cell_line}.csv"
     if not os.path.exists(file_path):
         st.write(f"DEGs not found for: {cell_line} with {drug}")
     else:
