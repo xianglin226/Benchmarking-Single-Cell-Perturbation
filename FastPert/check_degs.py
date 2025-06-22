03ce386f3cc257b646e00fb1a81ce55099fe6fce
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-21 22:11:00 -0400

    Update check_degs.py

--- FastPert/check_degs.py
+++ FastPert/check_degs.py
@@ -5,6 +5,10 @@ import os
 drug = st.text_input("Enter drug name: (e.g. pazopanib...)")
 cell_line = st.selectbox("Select Cell Line", ['A549','MCF7','PC3', 'VCAP', 'MDAMB231', 'BT20', 'HT29', 'A375', 'HELA'])
 type = st.selectbox("Select data type", ['chemical','genetic'])
+if type == 'chemical':
+    drug = drug.lower()
+elif type =='genetic':
+    drug = drug.upper()
 
 if st.button("Load DEGs"):
     file_path = f"/mount/src/benchmarking-single-cell-perturbation/FastPert/data/{type}_top20degs_{cell_line}.csv"
@@ -12,7 +16,7 @@ if st.button("Load DEGs"):
         st.write(f"DEGs not found for: {cell_line} with {type} perturbation")
     else:
         dat = pd.read_csv(file_path, index_col=0)
-        dat = dat[dat["condition"] == drug.lower()]
+        dat = dat[dat["condition"] == drug]
         dat = dat.sort_values(by="logfoldchanges", ascending=False)
         dat = dat[['names','logfoldchanges']]
         dat = dat.reset_index(drop=True)
