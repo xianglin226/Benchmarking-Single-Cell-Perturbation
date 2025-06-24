f1049ad4f618d1fc25497c9a8d033786b3b46a28
Xiang Lin <79429595+xianglin226@users.noreply.github.com>
2025-06-24 07:12:21 -0400

    Update check_pathways.py

--- FastPert/check_pathways.py
+++ FastPert/check_pathways.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
+import os
 
+drug = st.text_input("Enter perturbagen: (e.g. drug like pazopanib or knocked-out gene like CCL1)")
+cell_line = st.selectbox("Select Cell Line", ['A549','MCF7','PC3', 'VCAP', 'MDAMB231', 'BT20', 'HT29', 'A375', 'HELA', 'BICR6', 'YAPC', 'AGS', 'U251MG', 'ES2'])
+type = st.selectbox("Select data type", ['chemical'])
+if type == 'chemical':
+    drug = drug.lower()
+#elif type =='genetic':
+#    drug = drug.upper()
+
+if st.button("Load DEGs"):
+    file_path = f"/mount/src/benchmarking-single-cell-perturbation/FastPert/data/{type}_chemical_top10gsea_reactome_{cell_line}.csv"
+    if not os.path.exists(file_path):
+        st.write(f"DEGs not found for: {cell_line} with {type} perturbation")
+    else:
+        dat = pd.read_csv(file_path)
+        dat = dat[dat["Perturbation"] == drug]
+        dat = dat.sort_values(by="NES", ascending=False)
+        dat = dat.reset_index(drop=True)
+        st.write(f"Showing top 10 enriched genesets for cell line {cell_line} with {type} perturbagen: {drug}")
+        st.dataframe(dat)
